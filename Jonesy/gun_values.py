f162c6143c89130b7012cf69c66427b40d231e0a
MotasGoat <fovalle@cps.edu>
2023-10-13 09:54:03 -0500

    yur

---
+++ Jonesy/gun_values.py
@@ -0,0 +1,124 @@
+
+gun_stats = (
+   #gun 
+    [
+        "Tactical Assault Rifle", #name
+        "Legendary", #rarity
+        168, #DPS
+        24, #Dmg
+        24, #StructDmg
+        7, #FireRate
+        30, #MagSize
+        1.98, #Reload
+    ],
+   #gun 
+    [
+        "Havoc Suppressed Assault Rifle", #name
+        "Common", #rarity
+        126, #DPS
+        21, #Dmg
+        21, #StructDmg
+        6, #FireRate
+        30, #MagSize
+        2.75, #Reload
+    ],
+    #gun 
+    [
+        "Havoc Suppressed Assault Rifle", #name
+        "Uncommon", #rarity
+        132, #DPS
+        22, #Dmg
+        22, #StructDmg
+        6, #FireRate
+        30, #MagSize
+        2.63, #Reload
+    ],
+    #gun 
+    [
+        "Havoc Suppressed Assault Rifle", #name
+        "Rare", #rarity
+        138, #DPS
+        23, #Dmg
+        23, #StructDmg
+        6, #FireRate
+        30, #MagSize
+        2.5, #Reload
+    ],
+    #gun 
+    [
+        "Havoc Suppressed Assault Rifle", #name
+        "Epic", #rarity
+        144, #DPS
+        24, #Dmg
+        24, #StructDmg
+        6, #FireRate
+        30, #MagSize
+        2.38, #Reload
+    ],
+    #gun 
+    [
+        "Havoc Suppressed Assault Rifle", #name
+        "Legendary", #rarity
+        150, #DPS
+        25, #Dmg
+        25, #StructDmg
+        6, #FireRate
+        30, #MagSize
+        2.25, #Reload
+    ],
+    #gun 
+    [
+        "Twin Mag Assault Rifle", #name
+        "Common", #rarity
+        126, #DPS
+        21, #Dmg
+        21, #StructDmg
+        6, #FireRate
+        30, #MagSize
+        2.75, #Reload
+    ],
+    #gun 
+    [
+        "Twin Mag Assault Rifle", #name
+        "Uncommon", #rarity
+        132, #DPS
+        22, #Dmg
+        22, #StructDmg
+        6, #FireRate
+        30, #MagSize
+        2.63, #Reload
+    ],
+    #gun 
+    [
+        "Twin Mag Assault Rifle", #name
+        "Rare", #rarity
+        138, #DPS
+        23, #Dmg
+        23, #StructDmg
+        6, #FireRate
+        30, #MagSize
+        2.5, #Reload
+    ],
+    #gun 
+    [
+        "Twin Mag Assault Rifle", #name
+        "Epic", #rarity
+        144, #DPS
+        24, #Dmg
+        24, #StructDmg
+        6, #FireRate
+        30, #MagSize
+        2.38, #Reload
+    ],
+    #gun 
+    [
+        "Twin Mag Assault Rifle", #name
+        "Legendary", #rarity
+        150, #DPS
+        25, #Dmg
+        25, #StructDmg
+        6, #FireRate
+        30, #MagSize
+        2.25, #Reload
+    ],
+)
