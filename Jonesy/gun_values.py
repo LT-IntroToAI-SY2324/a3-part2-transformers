1530c868f5685b497d71172676ebbe1e8247ecf2
Moses <mmunoz69@cps.edu>
2023-10-18 10:28:14 -0500

    added DMPs

--- Jonesy/gun_values.py
+++ Jonesy/gun_values.py
@@ -126,5 +126,30 @@ gun_stats = (
         "Legendary", #rarity
         131.6, 94, 4.4, 1.4, 6, 4.73,
     ],
+    [
+        "Thermal DMR", #name
+        "Uncommon", #rarity
+        165, 33, 33, 5, 20, 3.68,
+    ],
+    [
+        "ThermalDMR",
+        "rare",
+        175, 35, 35, 5, 20, 3.5,
+    ],
+    [
+        "ThermalDMR",
+        "epic",
+        195, 39, 39, 5, 20, 3.15,
+    ],
+    [
+        "ThermalDMR",
+        "legendary"
+        195, 39, 39, 5,	20,	3.15,
+    ],
+    [
+        "ThermalDMR",
+        "mythical"
+        200, 40, 40, 5,	20,	2.98,
+    ]
        
 )
---
+++ temp_moses_gun_vals.py
@@ -0,0 +1,26 @@
+#gun
+[
+    "Thermal DMR", #name
+    "Uncommon", #rarity
+    165, 33, 33, 5, 20, 3.68,
+],
+[
+    "ThermalDMR",
+    "rare",
+    175, 35, 35, 5, 20, 3.5,
+],
+[
+    "ThermalDMR",
+    "epic",
+    195, 39, 39, 5, 20, 3.15,
+],
+[
+    "ThermalDMR",
+    "legendary"
+    195, 39, 39, 5,	20,	3.15,
+],
+[
+    "ThermalDMR",
+    "mythical"
+    200, 40, 40, 5,	20,	2.98,
+]
